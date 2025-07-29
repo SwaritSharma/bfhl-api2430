590b594119d74a7052fd55cd00e4b3a75556f9dc
SwaritSharma <swarit.developer@gmail.com>
2025-07-29 22:20:02 +0530

    Updated structure 13 for Vercel deployment and refactored bfhl.js

---
+++ api/bfhl.js
@@ -0,0 +1,57 @@
+// api/bfhl.js
+module.exports = (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  const { data } = req.body;
+
+  if (!Array.isArray(data)) {
+    return res.status(400).json({ is_success: false, message: "Invalid input" });
+  }
+
+  const userId = "swarit_sharma_1022002";
+  const email = "swarit2430.be22@chitkar.edu.in";
+  const rollNumber = "2210992430";
+
+  let evenNumbers = [];
+  let oddNumbers = [];
+  let alphabets = [];
+  let specialCharacters = [];
+  let sum = 0;
+  let concatString = '';
+
+  data.forEach(item => {
+    if (!isNaN(item)) {
+      const num = parseInt(item);
+      sum += num;
+      if (num % 2 === 0) {
+        evenNumbers.push(item);
+      } else {
+        oddNumbers.push(item);
+      }
+    } else if (/^[a-zA-Z]+$/.test(item)) {
+      alphabets.push(item.toUpperCase());
+      concatString += item;
+    } else {
+      specialCharacters.push(item);
+    }
+  });
+
+  concatString = concatString.split('').reverse().map((char, index) =>
+    index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()
+  ).join('');
+
+  return res.status(200).json({
+    is_success: true,
+    user_id: userId,
+    email: email,
+    roll_number: rollNumber,
+    even_numbers: evenNumbers,
+    odd_numbers: oddNumbers,
+    alphabets: alphabets,
+    special_characters: specialCharacters,
+    sum: sum.toString(),
+    concat_string: concatString
+  });
+};
